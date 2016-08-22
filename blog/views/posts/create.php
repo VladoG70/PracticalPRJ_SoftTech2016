15c1489be725d0206cd09988dc7bbbb744a78300
VladoG70 <shopping@radiovot.net>
2016-08-22 23:15:34 +0300

    Create.php
    add <br>

--- blog/views/posts/create.php
+++ blog/views/posts/create.php
@@ -7,7 +7,7 @@
     <input type="text" name="post_title" />
     <div>Content:</div>
     <textarea rows="10" name="post_content" ></textarea>
-    <div><input type="submit" value="Create" />
+    <div><input type="submit" value="Create" /><br>
         <a href="<?=APP_ROOT?>/posts">[Cancel]</a>
     </div>
 </form>
