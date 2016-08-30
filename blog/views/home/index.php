8ea785cc22145bab43f4b428c6219e4c2e35349f
VladoG70 <shopping@radiovot.net>
2016-08-31 01:09:12 +0300

    Fix CSS
    Fix CSS -> in \blog\views\home\index.php
    Add link to post_title -> to read single post

--- blog/views/home/index.php
+++ blog/views/home/index.php
@@ -25,7 +25,9 @@
         <?php foreach ($this->posts as $post) {?>
             <li class="single-post">
                 <div class="dot">&nbsp;</div>
+                <a href="<?=APP_ROOT?>/home/view/<?=$post['id']?>">
                 <h2 class="title"><?=htmlentities($post['title'])?></h2>
+                    </a>
                 <div class="subtitle"><i>Posted on</i>
                     <?=(new DateTime($post['date']))->format('d-M-Y')?>
                     <i>by</i> <?=htmlentities($post['full_name'])?>
